17ffcc886ca746930d14cecfec86270a99e764db
Svk636 <93348686+Svk636@users.noreply.github.com>
2026-02-25 22:09:48 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,1 @@
-import type { CapacitorConfig } from '@capacitor/cli';
 
-const config: CapacitorConfig = {
-  appId: 'com.o2elite.app',
-  appName: 'O2 Elite',
-  webDir: 'public'
-};
-
-export default config;
